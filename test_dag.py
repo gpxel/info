47c3c1467431b0eb10145108df6da836aed9b607
Sergey <69768621+gpxel@users.noreply.github.com>
2024-11-28 15:22:45 +0400

    Update test_dag.py

--- test_dag.py
+++ test_dag.py
@@ -8,7 +8,7 @@ def print_hello():
 
 dag = DAG('hello_world', description='Hello World DAG',
           schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+          start_date=datetime(2024, 3, 20), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
